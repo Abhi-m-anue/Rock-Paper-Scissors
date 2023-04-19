421265197eba5a1f41cdd8cceb00ebefdbf267fc
Abhi-m-anue <125118501+Abhi-m-anue@users.noreply.github.com>
2023-04-19 16:59:36 +0530

    improved design

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-
-let yc=document.querySelector(".yc");
-let cmc=document.querySelector(".cc");
 let userscore=document.querySelector('.usr-score');
 let comscore=document.querySelector('.com-score');
 let status=document.querySelector('.status');
 let pa=document.querySelector('.ply-agn');
-/*let r=document.querySelector('rock');
-let p=document.querySelector('paper');
-let s=document.querySelector('scissors');*/
+let score=document.querySelector('.score');
 
 let userpoint=0,compoint=0;
-comscore.innerHTML="Computer point:"+compoint;
-userscore.innerHTML="Your point: "+userpoint;
+comscore.innerHTML="SCORE :"+compoint;
+userscore.innerHTML="SCORE :"+userpoint;
 
 const btn=document.querySelectorAll(".btn");
     let playerChoice;
     btn.forEach(element => {
-    element.addEventListener('click',(e)=>{
-    if(e){
-        playerChoice=e.target.innerHTML.toLowerCase();
-        start(playerChoice);
-       
+    element.addEventListener('click',response)
+    });
+    function response(e){
+        if(e){
+            playerChoice=e.target.id.toLowerCase();
+            score.style.display= "flex";
+            start(playerChoice);
+        }
     }
-    })});
 
 function getComputerChoice(){
    let options=["Rock","Paper","Scissors"];
@@ -30,8 +27,7 @@ function getComputerChoice(){
    return options[x];
    console.log(options[x]);
 }
-function start(pc){
-    yc.innerHTML="Your Choice: "+pc;
+function start(pc){  
         let comChoice=getComputerChoice();
         let cc=comChoice.toLowerCase();
         let x=document.querySelector('.'+cc);  
@@ -39,19 +35,18 @@ function start(pc){
         setTimeout(() => {
             x.classList.remove('selected');
         }, 2000);
-        cmc.innerHTML="Computer Choice: "+cc;
         play(pc,cc);
 }
 function play(pc,cc){
     if(pc==="rock" && cc==="paper" || pc==="paper" && cc==="scissors" || pc==="scissors" && cc==="rock"){
         compoint++;
-        comscore.innerHTML="Computer point:"+compoint;
+        comscore.innerHTML="SCORE :"+compoint;
         status.innerHTML="You Loose.."+ cc +" beats "+ pc;
         //alert("You Loose.."+ cc +" beats "+ pc+"\n your point:"+userpoint+"  comp point:"+compoint);
     }
     else if(pc==="rock" && cc==="scissors" || pc==="paper" && cc==="rock" || pc==="scissors" && cc==="paper"){
         userpoint++;
-        userscore.innerHTML="Your point: "+userpoint;
+        userscore.innerHTML="SCORE :"+userpoint;
         status.innerHTML="You win.."+ pc +" beats "+cc;
         //alert("You win.."+ pc +" beats "+ cc+"\n your point:"+userpoint+"  comp point:"+compoint);
     }
@@ -60,35 +55,34 @@ function play(pc,cc){
        // alert("Draw match  \n your point:"+userpoint+"  comp point:"+compoint)
     }
     checkres(userpoint,compoint);
-    //console.log(playerChoice);
 }
 
 function checkres(up,cp){
     if(up===3){
         //alert("You win the game..\n your point:"+up+"  comp point:"+cp);
-        cmc.innerHTML='';
-        yc.innerHTML='';
-        comscore.innerHTML='';
-        userscore.innerHTML='';
-        userpoint=0,compoint=0;
+        btn.forEach(element => element.removeEventListener('click',response ));
         setTimeout(() => {
             status.innerHTML='YOU WON THE GAME';
             pa.classList.remove('ply-agn');
             pa.classList.add('enable');
+            comscore.innerHTML='';
+            userscore.innerHTML='';
+            userpoint=0,compoint=0;
+            score.style.display= "none";
             play_again();
-        }, 4000);
+            }, 4000);
     }
     else if(cp===3){
         //alert("You Loose the game..\n your point:"+userpoint+"  comp point:"+compoint);
-        cmc.innerHTML='';
-        yc.innerHTML='';
-        comscore.innerHTML='';
-        userscore.innerHTML='';
-        userpoint=0,compoint=0;
+        btn.forEach(element => element.removeEventListener('click',response ));
         setTimeout(() => {
             status.innerHTML='YOU LOST THE GAME';
             pa.classList.remove('ply-agn');
             pa.classList.add('enable');
+            comscore.innerHTML='';
+            userscore.innerHTML='';
+            userpoint=0,compoint=0;
+            score.style.display= "none";
             play_again();
         }, 4000);
     }
